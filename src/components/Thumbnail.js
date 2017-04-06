ab03bd7f9ea65c089022ce77dabda361067b148d
sfice16 <sfice16@gmail.com>
2017-04-06 21:52:21 +0900

    Add files via upload

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ThumbnailUpper from './ThumbnailUpper'
 import ThumbnailLower from './ThumbnailLower'
 
+const posts = [
+  {id:'post0', title:'board1', content:'good night'},
+  {id:'post1', title:'board2', content:'sleepyhead'},
+  {id:'post2', title:'board3', content:'hello'}
+];
+
 class Thumbnail extends React.Component {
     render(){
+      const renderPosts = posts.map((post) => {
+
+        return (
+          <ThumbnailUpper key={post.id} title={post.title} content2={post.content}/> /* 정보를 받아올때 변수는 {}로 wrapping해준다 */
+        )
+      });
+
         return (
           <div className="container">
             <div className="row">
-              <ThumbnailUpper/>
-              <ThumbnailUpper/>
-              <ThumbnailUpper/>
+              {renderPosts}
             </div>
             <div className="row">
               <ThumbnailLower/>
